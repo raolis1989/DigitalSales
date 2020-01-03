f23798ddb871e8c07a1a7621decebee1ea8362d4
Raolis <raolis1989@gmail.com>
2020-01-03 00:30:41 -0300

    new-class

---
+++ DigitalSales/DigitalSales.Entities/Users/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DigitalSales.Entities.Users
+{
+    public class Role
+    {
+        public int idRole { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
