e304221d0be01c4e41afcd5552a76dd13125f392
Raolis <raolis1989@gmail.com>
2020-01-05 20:03:26 -0300

    update-user

--- DigitalSales/DigitalSales.Web/Models/Users/User/UpdateViewModel.cs
+++ DigitalSales/DigitalSales.Web/Models/Users/User/UpdateViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,27 @@ namespace DigitalSales.Web.Models.Users.User
 {
     public class UpdateViewModel
     {
+
+        [Required]
+        public int IdUser { get; set; }
+
+        [Required]
+        public int IdRole { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "El nombre no debe de tener mas de 100 caracteres")]
+        public string Name { get; set; }
+        public string TypeDocument { get; set; }
+        public string NumDocument { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public bool act_password { get; set; }
     }
 }
