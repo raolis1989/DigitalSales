69e4c882122389e08df3625dc05940ce83ff14a1
Raolis <raolis1989@gmail.com>
2020-01-11 00:37:20 -0300

    repository-persons

--- DigitalSales/DigitalSales.Data/Interfaces/IPersonRepository.cs
+++ DigitalSales/DigitalSales.Data/Interfaces/IPersonRepository.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using DigitalSales.Entities.Sales;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DigitalSales.Data.Interfaces
 {
     public interface IPersonRepository
     {
+        Task<List<Person>> ObtainPersonsAsync();
+        Task<Person> ObtainPersonAsync(int id);
+        Task<Person> Agregar(Person category);
+        Task<bool> Actualizar(Person category);
+        Task<bool> Eliminar(int id);
+        Task<bool> Deactivate(int id);
+        Task<bool> Activate(int id);
+        Task<List<Person>> ObtainCategoriesActiveAsync();
     }
 }
--- DigitalSales/DigitalSales.Data/Repository/PersonRepository.cs
+++ DigitalSales/DigitalSales.Data/Repository/PersonRepository.cs
@@ -1,14 +1,53 @@
-﻿using System;
+﻿using DigitalSales.Data.Interfaces;
+using DigitalSales.Entities.Sales;
+using System;
 using System.Collections.Generic;
 
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DigitalSales.Data.Repository
 {
-    public class PersonRepository
+    public class PersonRepository : IPersonRepository
     {
+        public Task<bool> Activate(int id)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task<bool> Actualizar(Person category)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task<Person> Agregar(Person category)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task<bool> Deactivate(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Eliminar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Person>> ObtainCategoriesActiveAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Person> ObtainPersonAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Person>> ObtainPersonsAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
