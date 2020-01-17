9dd3a8365902bbc5f0a20f6ba41da04d66179570
Raolis <raolis1989@gmail.com>
2020-01-17 18:17:36 -0300

    controller-person

--- DigitalSales/DigitalSales.Web/Controllers/PersonsController.cs
+++ DigitalSales/DigitalSales.Web/Controllers/PersonsController.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using DigitalSales.Data.Interfaces;
+using DigitalSales.Web.Models.Sales.Person;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +15,33 @@ namespace DigitalSales.Web.Controllers
     [ApiController]
     public class PersonsController : ControllerBase
     {
+        private readonly IMapper _mapper;
+        private readonly IPersonRepository _personRepository;
+
+        public PersonsController(IMapper mapper, IPersonRepository personRepository)
+        {
+            _mapper = mapper;
+            _personRepository = personRepository;
+        }
+
+
+        [HttpGet("[action]")]
+        [EnableCors()]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<IEnumerable<PersonViewModel>>> List()
+        {
+
+            try
+            {
+                var person = await _personRepository.ObtainPersonsAsync();
+
+                return _mapper.Map<List<PersonViewModel>>(person);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
     }
 }
---
+++ DigitalSales/DigitalSales.Web/Models/Sales/Person/PersonViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalSales.Web.Models.Sales.Person
+{
+    public class PersonViewModel
+    {
+        public int IdPerson { get; set; }
+        public string Type_person { get; set; }
+        public string Name { get; set; }
+        public string Type_document { get; set; }
+        public string Num_document { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public bool Condition { get; set; }
+
+    }
+}
