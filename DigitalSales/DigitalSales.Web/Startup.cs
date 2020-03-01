16bbcde4312b0a83206ace71bae55ad32a551046
Raolis <raolis1989@gmail.com>
2020-03-01 02:06:03 -0300

    authorization

--- DigitalSales/DigitalSales.Web/Startup.cs
+++ DigitalSales/DigitalSales.Web/Startup.cs
@@ -70,7 +70,10 @@ namespace DigitalSales.Web
 
             app.UseRouting();
 
+
+            app.UseAuthentication();
             app.UseAuthorization();
+
 
             app.UseEndpoints(endpoints =>
             {
